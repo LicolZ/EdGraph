bb57e5fa60d54b37e7079a428cfb0e7078102e69
Licol Zeinfeld <licol@berkeley.edu>
2023-08-03 10:09:49 -0700

    django server deployed and running

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -18,17 +18,30 @@ class ProcessFileView(APIView):
             print(f'Received file with name {file.name}')
             # Save the file temporarily
             file_path = default_storage.save('temp.pdf', file)
+            print(f'saved the file temp {file.name}')
             # Process the file
 
             process_file = ProcessFile(file_path)
+            print(f'Processed the file {file.name}')
 
             # get topics
             topics = process_file.extract_topics()
+            print(f'got topics {topics}')
 
             # get relationships between topics
             relationships = process_file.fetch_topic_relationships(topics)
+            print(f'relationships {topics}')
 
             # Return the topics and relationships in a JSON response
             return JsonResponse({"topics": topics, "relationships": relationships})
         else:
             return Response({'error': 'No file provided'}, status=400)
+        
+class HealthCheckFileView(APIView):
+    parser_class = (FileUploadParser)
+
+    def post(self, request, *args, **kwargs):
+        print("hello world version 1.0!")
+
+        return JsonResponse({"hello": "world"})
+        
--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import nltk
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings." + os.environ.get("DJANGO_ENV", "development"))
+
+nltk.download('punkt')
 
 application = get_wsgi_application()
